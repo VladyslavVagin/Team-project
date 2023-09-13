7ee1e67c574338faca74a8eb9e7ceb3c8cc1cf8b
VladyslavVagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-09-13 12:52:27 +0200

    fgdfsgdf

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,20 @@
 (() => {
-   const refs = {
-      openModalBtn: document.querySelector('[data-menu-open]'),
-      closeModalBtn: document.querySelector('[data-menu-close]'),
-      modal: document.querySelector('[data-menu]'),
+   const mobileMenu = document.querySelector('.js-menu-container');
+   const openMenuBtn = document.querySelector('.js-open-menu');
+   const closeMenuBtn = document.querySelector('.js-close-menu');
+
+   const toggleMenu = () => {
+      const isMenuOpen =
+         openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+      openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+      mobileMenu.classList.toggle('is-hidden');
+
+      const scrollLockMethod = !isMenuOpen
+         ? 'disableBodyScroll'
+         : 'enableBodyScroll';
+      bodyScrollLock[scrollLockMethod](document.body);
    };
 
-   refs.openModalBtn.addEventListener('click', toggleModal);
-   refs.closeModalBtn.addEventListener('click', toggleModal);
-
-   function toggleModal() {
-      refs.modal.classList.toggle('is-hidden');
-   }
+   openMenuBtn.addEventListener('click', toggleMenu);
+   closeMenuBtn.addEventListener('click', toggleMenu);
 })();
