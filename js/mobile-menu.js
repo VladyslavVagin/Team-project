4175bda2fa9044b63f35f02c6fe2cefbdc847426
VladyslavVagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-09-13 11:23:58 +0200

    fgfg

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,20 +1,14 @@
 (() => {
-   const mobileMenu = document.querySelector('.js-menu-container');
-   const openMenuBtn = document.querySelector('.js-open-menu');
-   const closeMenuBtn = document.querySelector('.js-close-menu');
-
-   const toggleMenu = () => {
-      const isMenuOpen =
-         openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-      openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-      mobileMenu.classList.toggle('is-hidden');
-
-      const scrollLockMethod = !isMenuOpen
-         ? 'disableBodyScroll'
-         : 'enableBodyScroll';
-      bodyScrollLock[scrollLockMethod](document.body);
+   const refs = {
+      openModalBtn: document.querySelector('[data-menu-open]'),
+      closeModalBtn: document.querySelector('[data-menu-close]'),
+      modal: document.querySelector('[data-menu]'),
    };
 
-   openMenuBtn.addEventListener('click', toggleMenu);
-   closeMenuBtn.addEventListener('click', toggleMenu);
+   refs.openModalBtn.addEventListener('click', toggleModal);
+   refs.closeModalBtn.addEventListener('click', toggleModal);
+
+   function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+   }
 })();
